5bd2af662fff66adf74956808452dbe598e5b5b3
Rahulstar177 <rathodsachiit@gmaiil.com>
2024-09-24 19:54:41 +0530

    tes

--- src/main/java/com/security/service/SecurityConfig.java
+++ src/main/java/com/security/service/SecurityConfig.java
@@ -29,7 +29,23 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+
+    
+   }
+
+
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable())
+                .authorizeRequests().
+                requestMatchers("/test").authenticated().requestMatchers("/auth/login").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().exceptionHandling(ex -> ex.authenticationEntryPoint(point))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
-
-
 }
